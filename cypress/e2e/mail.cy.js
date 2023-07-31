99917eb6272d44dc18dcde50d99932abceb2a414
Pokfinner <finkry@gmail.com>
2023-07-31 20:25:34 +0100

    teste

---
+++ cypress/e2e/mail.cy.js
@@ -0,0 +1,29 @@
+
+
+describe('Test e-mail sending', () => {
+
+
+    it('should login', () => {
+
+
+        cy.on('uncaught:exception', (err, runnable) => {
+
+            return false
+
+        })
+
+        cy.visit("mail.sapo.pt")
+
+
+        cy.get('#mailIcon').click()
+
+
+        cy.get('sapo_widget_login_form input[type="text"]').type("testito")
+
+
+
+    })
+
+
+
+})
